e093374533311a93877ed695b575cc3273494aa1
CodingRookieSty <sty950114@163.com>
2017-07-30 22:19:57 +0800

    use string type to simplify code
    使用string类型变量来简化2-1代码

--- Chapter-2/2-1.cpp
+++ Chapter-2/2-1.cpp
@@ -1,34 +1,12 @@
 #include <iostream>
-#include <malloc.h>
-#include <string.h>
+#include <string>
 
-using std::cin;
-using std::cout;
-using std::endl;
-
-#define MAX_NAME_LEN 10
-#define MAX_ADDRESS_LEN 50
+using namespace std;
 
 int main()
 {
-    char* name;
-    char* address;
-
-    name = (char*)malloc(sizeof(MAX_NAME_LEN));
-    address = (char*)malloc(sizeof(MAX_ADDRESS_LEN));
-
-    if (NULL == name)
-    {
-        cout << "allocate name memory fail！" << endl;
-        return -1;
-    }
-
-    if (NULL == address)
-    {
-        cout << "allcoate address memory fail!" << endl;
-        free(name);
-        return -1;
-    }
+    string name;
+    string address;
 
     cout << "please enter your name:" << endl;
     cin >> name;
@@ -36,28 +14,8 @@ int main()
     cout << "please enter your address:" << endl;
     cin >> address;
 
-    if (strlen(name) > MAX_NAME_LEN)
-    {
-        cout << "name is too long" << endl;
-        free(name);
-        free(address);
-        return -1;
-    }
-
-    if (strlen(address) > MAX_NAME_LEN)
-    {
-        cout << "address is too long" << endl;
-        free(name);
-        free(address);
-        return -1;
-    }
-
     cout << name << endl;
     cout << address << endl;
 
-    free(name);
-    free(address);
-
     return 0;
 }
-
